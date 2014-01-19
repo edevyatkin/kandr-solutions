d7e8f4c1d765c405ba58b35f95524b9683881a84
Evgeniy Devyatkin <edevyatkin@dialog.nirhtu.ru>
2014-01-19 20:26:15 +0400

    Исправил решение задачи 1-20, заготовка функции

--- solutions/1_20.c
+++ solutions/1_20.c
@@ -16,21 +16,38 @@ int main()
     i = spaces = 0;
     
     while ((c = getchar()) != EOF) {
-        ++i;
-        if (c == '\t' && (spaces = (i/COLS + 1)*COLS - i)) {
+        if (c == '\t') {
+            spaces = (i/COLS + 1)*COLS - i;
             while (spaces > 0) {
                 putchar(' ');
                 ++i;
                 --spaces;
             }
         }
-        else
+        else {
             putchar(c);
+            ++i;
+        }
     }
-
+    
     return 0;
 }
 
 void detab(char s[])
 {
+    int i;
+    i = 0;
+    
+    while (s[i] != '\0') {
+        if (s[i] == '\t') {
+            spaces = (i/COLS + 1)*COLS - i;
+            while (spaces > 0) {
+                s[i] = ' ';
+                ++i;
+                --spaces;
+            }
+        }
+        else 
+            ++i;
+    }
 }
