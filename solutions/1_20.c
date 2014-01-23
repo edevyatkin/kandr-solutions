c847a86934bcf60771aa9558fc71a1b01fdf6727
Evgeniy Devyatkin <edevyatkin@dialog.nirhtu.ru>
2014-01-23 21:10:15 +0400

    Доработка задачи 1-20. Добавил функцию getline_lim

--- solutions/1_20.c
+++ solutions/1_20.c
@@ -6,37 +6,40 @@
 #define MAXLINE 1000    /* максимальная длина строки в потоке */
 #define COLS 8 
 
+int getline_lim(char line[], int maxline);
+void detab(char s[]);
+
 /*  */
 int main()
 {
-    int i;
-    int spaces;
-    char c;
+    char s[MAXLINE];
     
-    i = spaces = 0;
-    
-    while ((c = getchar()) != EOF) {
-        if (c == '\t') {
-            spaces = (i/COLS + 1)*COLS - i;
-            while (spaces > 0) {
-                putchar(' ');
-                ++i;
-                --spaces;
-            }
-        }
-        else {
-            putchar(c);
-            ++i;
-        }
+    while (getline_lim(s,MAXLINE) > 0) {
+        detab(s);
+        printf("%s", s);
     }
     
     return 0;
 }
 
+int getline_lim(char s[], int lim)
+{
+    int c, i;
+    
+    for (i = 0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
 void detab(char s[])
 {
-    int i;
-    i = 0;
+    int i, spaces;
+    i = spaces = 0;
     
     while (s[i] != '\0') {
         if (s[i] == '\t') {
